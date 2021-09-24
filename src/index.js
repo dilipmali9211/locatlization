851f282956af8477fe810fc72b334e5a435ce0ef
dilipmali9211 <dilipmali9211@gmail.com>
2021-09-24 18:05:55 +0530

    locallization

--- src/index.js
+++ src/index.js
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import {  initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector'
+import HttpApi from 'i18next-http-backend'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(LanguageDetector)
+  .use(HttpApi)
+  .init({
+    supportedLngs:['en','gj','hi','tm'],
+    fallbackLng: "en",
+    detection: {
+      order: [ 'cookie','htmlTag', 'localStore', 'path', 'subdomain'],
+      caches: ['cookie'],
+    },
+    backend: {
+      loadPath: '/assets/locales/{{lng}}/translation.json',
+    },
+    react:{useSuspense:false},
+  });
+
+
+ReactDOM .render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
